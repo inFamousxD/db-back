3a0d5fce0d84a4a8a9ea7fba67790cbd09ef9d46
inFamousxD <audimankar@gmail.com>
2024-04-25 14:54:36 -0400

    Completion

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
-const mongoose = require('mongoose');
 const app = require('./app');
-const config = require('./config/config');
-const logger = require('./config/logger');
 
-server = app.listen(config.port, () => {
-  logger.info(`Listening to port ${config.port}`);
+server = app.listen(3000, () => {
+  console.log('Running on 3000')
 });
 
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info('Server closed');
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
-
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
-});
